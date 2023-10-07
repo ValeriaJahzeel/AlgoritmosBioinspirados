2c89acc4d14d066b8b87dd947f841425134b8fab
Valeria Castañon <49892759+ValeriaJahzeel@users.noreply.github.com>
2023-10-07 00:26:42 -0600

    cambios finales

--- algoritmoGenetico.m
+++ algoritmoGenetico.m
@@ -1,16 +1,16 @@
-% 22 - septiembre - 2023
+% 6 - octubre - 2023
 % Valeria Jahzeel Castañon Hernandez
 % Prorgama de un algoritmo genético finalizado
 
 clc; % para limpiar la consola cada iteracion
 
 % se definen las variables
-poblacion = 10;
+poblacion = 20;
 variables = 2;
-ls = [1,1];
-li = [-1,-1];
+ls = [3,3];
+li = [-3,-3];
 press = [3,3];
-generaciones = 3;
+generaciones = 5;
 
 % ******************************************************************************
 % -------------------- PARTE 1 / GENERACION DE LOS INDIVIDUOS ------------------
@@ -34,39 +34,25 @@ for j = 1:variables
     columna_actual = columna_actual + Nb; % avanzar a la siguiente columna en la matriz
 end
 
-% suma_individuo = zeros(poblacion, 1);  % para almacenar las sumas
-%     
-% for i = 1:poblacion
-%     for j = 1:variables
-%        suma_individuo(i) = suma_individuo(i) + valores_reales(i, j)^2;
-%     end
-% end
-
+ % Imprimir el número total de bits utilizados
+ fprintf("Número total de bits utilizados: %d\n", num_bits_totales);
+    
+% --------------- EMPIEZAN LAS GENERACIONES --------------------
 for g = 1:generaciones
     fprintf("Generacion: %d \n", g);
+    disp(matriz);   % con los valores binarios
 
 
-
-    % Imprimir la matriz de valores reales
-    fprintf("Matriz de valores reales:\n");
-    disp(valores_reales);
+%     % Imprimir la matriz de valores reales
+%     fprintf("Matriz de valores reales:\n");
+%     disp(valores_reales);
+%     
+%     % Imprimir la matriz de arreglos binarios
+%     fprintf("Matriz de arreglos binarios:\n");
+%     disp(matriz);
     
-    % Imprimir la matriz de arreglos binarios
-    fprintf("Matriz de arreglos binarios:\n");
-    disp(matriz);
-    
-    % Imprimir el número total de bits utilizados
-    % fprintf("Número total de bits utilizados: %d\n", num_bits_totales);
-    
-    %Imprimir la suma de cuadrados de cada individuo
-    % fprintf("Suma de cuadrados de cada individuo:\n");
-    % disp(suma_individuo);
-    
-    % final = cat(2,valores_reales, suma_individuo);
-    % fprintf("Nueva matriz generada: \n");
-    % disp(final);
-    
-    fprintf("-------------------------------------------------------------")
+   
+    fprintf("-------------------------------------------------------------------------------------------------------------------------------------+\n")
     
     
     % ***********************************************************************
@@ -77,109 +63,60 @@ for g = 1:generaciones
     % separa las columnas (variables) de los valores reales
     x = valores_reales(:, 1);
     y = valores_reales(:, 2);
-    % x =1;
-    % y = 1;
     
     % funcion objetivo
     fx = 3 * (1 - x).^2 .* exp(-x.^2 - (y + 1).^2) + 10 * (x / 5 - x.^3 - y.^5) .* exp(-x.^2 - y.^2) - 1/3 * exp(-((x + 1).^2) - y.^2);
     
-    % Imprime los resultados de la funcion objetivo evaluada en los valores
-    % reales
+    % Imprime los resultados de la funcion objetivo evaluada en los valores reales
     % fprintf('Funcion objetivo: ');
     % disp(fx);
     
-    
-    % ------------- METODO DE LA RULETA -----------
-    % 1. Calcular el acumulado de la funcion objetivo acumulado=sumatoria(fx)
-    acumulado = sum(fx);
-    
-    % fprintf("Acumulado de fx: ");
-    % disp(acumulado);
-    
     % ¿se requieren ngeativos?
-    %  pi2 = negativosPositivos(fx); 
-    
-    % 2. Calcular la posibilidad de seleccion de cada individuo
-    pii = fx/acumulado;
-    
-    % fprintf("Posibilidad de seleccion: ");
-    % disp(pii);
-    
-    % 3. Calcular la probabilidad acumulada de cada individuo
-    qi =zeros(length(pii),1);
-    qi(1) = pii(1) ;
-    
-    for i = 2:length(pii)
-        qi(i) = qi(i - 1) + pii(i);
-    end
-    
-    % fprintf("Probabilidad acumulada: ");
-    % disp(qi);
+    qi = metodoRuleta(fx); 
     
     
+    % continuacion del metodo de la ruleta .....
     % 4. Determinar los padres mediante el método de la ruleta
-    padres = 6;  % número de padres que se quiere seleccionar 
+    padres = poblacion/2;  % número de padres que se quiere seleccionar 
     elegidos = zeros(padres, 1);  % arreglo con los indices de los padres
     i = 1;  % Inicializar i en 1
-    matPadres = zeros(padres,num_bits_totales);
     
     while i <= padres
         nuevo_padre = elegirPadre(qi);
         
         % verificar si ya se seleccionó
         if ~ismember(nuevo_padre, elegidos)
-            elegidos(i) = nuevo_padre;
+            elegidos(i) = nuevo_padre;  % se guarda la posicion del padre
             i = i + 1;  % se aumenta cuando se añade un padre
         end
     end
-    
-    % fprintf("Padres seleccionados:");
-    % disp(elegidos);
-    
-    % se muestran los padres elegidos
-    for i = 1:padres
-        indice = elegidos(i);
-        valor_q = qi(indice);
-        fprintf('I: %d, Q: %.3f\n', indice, valor_q);
-        matPadres(i,:) = matriz(indice,:);
-        disp(matPadres(i,:));
-    end
-    
-    
     
     % ************************************************************************
     % ------------------------- PARTE 3 / CRUZA ------------------------------
     % ************************************************************************
     hijos = zeros(poblacion, num_bits_totales);
-    
-    
+    matPadres = zeros(padres,2);
+
     for i = 1:2:padres
         % elige los padres aleatoriamente
         indices = randi([1,padres],2,1);    % elige 2 indices para los padres
-        if(indices(1) == indices(2))
+        if indices(1) == indices(2)
             indices = randi([1,padres],2,1);    % elige 2 indices para los padres
-        else
+        elseif indices(1) ~= indices(2) 
             padre1 = elegidos(indices(1));
             padre2 = elegidos(indices(2));
+            matPadres(i,:) = [indices(1), indices(2)];   % guarda los indices en la matriz
+        end      
+        
+        if ~ismember(indices(1), matPadres(:,1)) && ~ismember(indices(2), matPadres(:,2))   % para que no se repitan los padres
+            punto_cruza = randi([1, num_bits_totales - 1]); % calcula aleatoriamente el punto de cruza
+    
+            hijo1 = [matriz(padre1, 1:punto_cruza), matriz(padre2, punto_cruza + 1:end)];
+            hijo2 = [matriz(padre2, 1:punto_cruza), matriz(padre1, punto_cruza + 1:end)];
+            
+            hijos(i, :) = hijo1; 
+            hijos(i + 1, :) = hijo2;
         end
-    
-        
-    
-        punto_cruza = randi([1, num_bits_totales - 1]);
-        
-        fprintf("Punto de cruza \n")
-        disp(punto_cruza)
-        
-        fprintf("Matriz del padre 1 y del padre 2 \n")
-        disp(matriz(padre1,:));
-        disp(matriz(padre2,:));
-
-        hijo1 = [matriz(padre1, 1:punto_cruza), matriz(padre2, punto_cruza + 1:end)];
-        hijo2 = [matriz(padre2, 1:punto_cruza), matriz(padre1, punto_cruza + 1:end)];
-        
-        hijos(i, :) = hijo1; 
-        hijos(i + 1, :) = hijo2;
-      
     end
     
     % ahora la matriz corresponde a la nueva generacion
@@ -193,8 +130,8 @@ for g = 1:generaciones
     
     for i = 1:poblacion
         for j = 1:num_bits_totales
-            if rand < probabilidad_mutacion
-                if matriz(i,j) == 0
+            if rand < probabilidad_mutacion % si el valor es menor a la probabilidad
+                if matriz(i,j) == 0 % muta a los hijos
                     matriz(i,j) = 1;
                 else
                     matriz(i,j) = 0;
@@ -203,12 +140,8 @@ for g = 1:generaciones
         end
     end
     
-    % La poblacion final final
-    disp(matriz);
-
     valores_reales = calcularValoresReales(matriz,li,ls,press, valores_reales);
 end
-
 
 
 
@@ -246,10 +179,6 @@ function valores_reales = calcularValoresReales(matriz, li, ls, press, valores_r
     end
 end
 
-
-
-
-
 function indice = elegirPadre(qi)
     r = rand;  % valor de aptitud
     for j = 1:length(qi)
@@ -268,25 +197,25 @@ function decimal = binarioDecimal(binario)
 end
 
 % por si no se quieren negativos
-function positivos = negativosPositivos(fx)
-    fprintf("****************************");
+function qi = metodoRuleta(fx)
+ % ------------- METODO DE LA RULETA -----------
+ % 0. Aplicar la formula de normalizacion
     positivos = abs(fx/(min(fx+1)));
-    fprintf("Valores de fx positivos: ");
-    disp(positivos);
+
+% 1. Calcular el acumulado de la funcion objetivo acumulado=sumatoria(fx)
     acumulado = sum(positivos);
-    fprintf("Acumulado de fx: ");
-    disp(acumulado);
+
+% 2. Calcular la posibilidad de seleccion de cada individuo
     pii = positivos/acumulado;
-    fprintf("Posibilidad de seleccion ");
-    disp(pii);
+
+% 3. Calcular la probabilidad acumulada de cada individuo
     qi =zeros(length(pii),1);
     qi(1) = pii(1) ;
     for i = 2:length(pii)
         qi(i) = qi(i - 1) + pii(i);
     end
-    fprintf("Probabilidad acumulada: ");
-    disp(qi);
-    fprintf("****************************");
+
+% sigue en la parte principal ....
 end
 
 
